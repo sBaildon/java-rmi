a9a8d88a445349f518a61af5fd08087ff2a1ab3f
Sean Baildon <sean@sbaildon.com>
2013-10-15 00:24:32 +0100

    got an early version of pulling the spec

--- src/Requester.java
+++ src/Requester.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.rmi.Naming;
 import java.util.Random;
@@ -5,20 +9,29 @@ import java.util.Random;
 public class Requester implements Serializable {
 	final static int UID = 32877315;
 	final static int PASSCODE = 1340426414;
-	final static String SERVER = "scc311-server.lancs.ac.uk/";
+	final static String SERVER = "scc311-server.lancs.ac.uk";
 	final static String SERVER_NAME = "CW_server";
 
 	public static void main(String[] args) {
-		Client_request request;
 		CW_server_interface serv;
-		Server_response doc;
+		Client_request request;
+		Server_response response;
+		File file = new File("E:/spec.doc");
+		OutputStream doc = null;
+		try {
+			doc = new FileOutputStream(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		try {
-			serv = (CW_server_interface) Naming.lookup("rmi://" + SERVER + SERVER_NAME);
+			serv = (CW_server_interface) Naming.lookup("rmi://" + SERVER + "/" + SERVER_NAME);
 			request = GenerateRequest();
-			doc = serv.getSpec(UID, request);
+			response = serv.getSpec(UID, request);
+			response.write_to(doc);
 		} catch (Exception re) {
-			System.out.println("RemoteException"+re);			
+			System.out.println("RemoteException " + re);			
 		} 
 	}
 	
