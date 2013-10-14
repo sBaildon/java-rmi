a9612863a8ccba5a33c3e67f289f89c15b32342b
Sean Baildon <sean@sbaildon.com>
2013-10-14 22:46:56 +0100

    added nonse generator

---
+++ src/Requester.java
@@ -0,0 +1,15 @@
+import java.rmi.*;
+import java.util.Random;
+
+public class Requester {
+
+	public static void main(String[] args) {
+
+	}
+	
+	int GenerateNonse() {
+		Random rand = new Random();
+		return rand.nextInt();
+	}
+
+}
